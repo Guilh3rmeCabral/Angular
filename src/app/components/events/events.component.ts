951e647eb4c5c157ac804a0ddb3e3c4730d18091
Guilherme Cabral <guicabralprowork@gmail.com>
2023-03-14 20:17:11 -0300

    refactor: remoção de linhas comentandas

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -9,7 +9,6 @@ export class EventsComponent {
   show: boolean = false;
 
   showMessage(): void {
-    //Só vai tipificar o método void se ele não retornar nada.
-    this.show = !this.show; // A mesma coisa que trocar o valor de false para true. toggle
+    this.show = !this.show;
   }
 }
