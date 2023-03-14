5c95c67f9f5f9053c40828c294e28698f0b62d5f
Guilherme Cabral <guicabralprowork@gmail.com>
2023-03-14 20:11:59 -0300

    feat: criando botão para exibição de uma mensagem utilizando eventos e diretivas

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
+  show: boolean = false;
 
+  showMessage(): void {
+    //Só vai tipificar o método void se ele não retornar nada.
+    this.show = !this.show; // A mesma coisa que trocar o valor de false para true. toggle
+  }
 }
