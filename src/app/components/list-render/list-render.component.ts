b38e3fc3cc82ffd4e28e21f2f3614696cd730cba
Guilherme Cabral <guicabralprowork@gmail.com>
2023-03-18 16:10:19 -0300

    feat: importanto e usando as funcionalidades da interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,23 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.scss']
 })
 export class ListRenderComponent {
+  animalDetails = '';
 
-  animals = [
+  animals: Animal[] = [
     {
       name: "Turca",
-      type: "Dog"
+      type: "Dog",
+      age: 4
     },
     {
       name: "Tom",
-      type: "Cat"
+      type: "Cat",
+      age: 5
     },
     {
       name: "Frida",
-      type: "Dog"
+      type: "Dog",
+      age: 6
     },
     {
       name: "Bob",
-      type: "Horse"
+      type: "Horse",
+      age: 7
     }
   ]
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
 }
