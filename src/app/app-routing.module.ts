2bf19ae61532d40458bebe2229c3c1dbf8ad76be
Guilherme Cabral <guicabralprowork@gmail.com>
2023-03-29 18:53:54 -0300

    Feat: Adicionando rota dinamica para o componente ItemDetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 const routes: Routes = [
   {path: '', component: FirstComponentComponent},
   {path: 'list', component: ListRenderComponent},
-  {path: 'two', component: TwoWayBindingComponent}
+  {path: 'two', component: TwoWayBindingComponent},
+  {path: 'list/:id', component: ItemDetailsComponent},
   
 ];
 
